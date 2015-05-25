254a48eac02ba2e06aac9c4b909c74843080303b
Bruce Lane <bruce@batchass.fr>
2015-05-25 20:52:42 +0200

    changes..

--- addons.make
+++ addons.make
@@ -1,1 +1,2 @@
 ofxLibwebsockets
+ofxMidi
--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,6 +2,7 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	ofSetVerticalSync(true);
     // setup a server with default options on port 9092
     // - pass in true after port to set up with SSL
     //bSetup = server.setup( 9093 );
@@ -10,12 +11,32 @@ void ofApp::setup(){
     options.port = 9092;
 	options.bUseSSL = false; // you'll have to manually accept this self-signed cert if 'true'!
     bSetup = server.setup( options );
-    
+
     // this adds your app as a listener for the server
     server.addListener(this);
-    
+
     ofBackground(0);
     ofSetFrameRate(60);
+
+	// midi
+	// print input ports to console
+	midiIn.listPorts(); // via instance
+	//ofxMidiIn::listPorts(); // via static as well
+
+	// open port by number (you may need to change this)
+	midiIn.openPort(1);
+	//midiIn.openPort("IAC Pure Data In");	// by name
+	//midiIn.openVirtualPort("ofxMidiIn Input"); // open a virtual port
+
+	// don't ignore sysex, timing, & active sense messages,
+	// these are ignored by default
+	midiIn.ignoreTypes(false, false, false);
+
+	// add ofApp as a listener
+	midiIn.addListener(this);
+
+	// print received messages to the console
+	midiIn.setVerbose(true);
 }
 
 //--------------------------------------------------------------
@@ -26,30 +47,68 @@ void ofApp::update(){
 void ofApp::draw(){
     if ( bSetup ){
         ofDrawBitmapString("WebSocket server setup at "+ofToString( server.getPort() ) + ( server.usingSSL() ? " with SSL" : " without SSL"), 20, 20);
-        
+
         ofSetColor(150);
-        ofDrawBitmapString("Click anywhere to open up client example", 20, 40);  
+        ofDrawBitmapString("Click anywhere to open up client example", 20, 40);
     } else {
         ofDrawBitmapString("WebSocket setup failed :(", 20,20);
     }
-    
+
     int x = 20;
     int y = 100;
-    
+
     ofSetColor(0,150,0);
     ofDrawBitmapString("Console", x, 80);
-    
+
     ofSetColor(255);
     for (int i = messages.size() -1; i >= 0; i-- ){
         ofDrawBitmapString( messages[i], x, y );
         y += 20;
     }
     if (messages.size() > NUM_MESSAGES) messages.erase( messages.begin() );
-    
+
     ofSetColor(150,0,0);
     ofDrawBitmapString("Type a message, hit [RETURN] to send:", x, ofGetHeight()-60);
     ofSetColor(255);
     ofDrawBitmapString(toSend, x, ofGetHeight() - 40);
+	// midi
+	// draw the last recieved message contents to the screen
+	text << "Received: " << ofxMidiMessage::getStatusString(midiMessage.status);
+	ofDrawBitmapString(text.str(), 20, 20);
+	text.str(""); // clear
+
+	text << "channel: " << midiMessage.channel;
+	ofDrawBitmapString(text.str(), 20, 34);
+	text.str(""); // clear
+
+	text << "pitch: " << midiMessage.pitch;
+	ofDrawBitmapString(text.str(), 20, 48);
+	text.str(""); // clear
+	ofRect(20, 58, ofMap(midiMessage.pitch, 0, 127, 0, ofGetWidth()-40), 20);
+
+	text << "velocity: " << midiMessage.velocity;
+	ofDrawBitmapString(text.str(), 20, 96);
+	text.str(""); // clear
+	ofRect(20, 105, ofMap(midiMessage.velocity, 0, 127, 0, ofGetWidth()-40), 20);
+
+	text << "control: " << midiMessage.control;
+	ofDrawBitmapString(text.str(), 20, 144);
+	text.str(""); // clear
+	ofRect(20, 154, ofMap(midiMessage.control, 0, 127, 0, ofGetWidth()-40), 20);
+
+	text << "value: " << midiMessage.value;
+	ofDrawBitmapString(text.str(), 20, 192);
+	text.str(""); // clear
+	if(midiMessage.status == MIDI_PITCH_BEND) {
+		ofRect(20, 202, ofMap(midiMessage.value, 0, MIDI_MAX_BEND, 0, ofGetWidth()-40), 20);
+	}
+	else {
+		ofRect(20, 202, ofMap(midiMessage.value, 0, 127, 0, ofGetWidth()-40), 20);
+	}
+
+	text << "delta: " << midiMessage.deltatime;
+	ofDrawBitmapString(text.str(), 20, 240);
+	text.str(""); // clear
 }
 
 //--------------------------------------------------------------
@@ -77,21 +136,21 @@ void ofApp::onIdle( ofxLibwebsockets::Event& args ){
 //--------------------------------------------------------------
 void ofApp::onMessage( ofxLibwebsockets::Event& args ){
     cout<<"got message "<<args.message<<endl;
-    
+
     // trace out string messages or JSON messages!
     if ( !args.json.isNull() ){
         messages.push_back("New message: " + args.json.toStyledString() + " from " + args.conn.getClientName() );
     } else {
         messages.push_back("New message: " + args.message + " from " + args.conn.getClientName() );
     }
-        
+
     // echo server = send message right back!
     args.conn.send( args.message );
 }
 
 //--------------------------------------------------------------
 void ofApp::onBroadcast( ofxLibwebsockets::Event& args ){
-    cout<<"got broadcast "<<args.message<<endl;    
+    cout<<"got broadcast "<<args.message<<endl;
 }
 
 //--------------------------------------------------------------
@@ -110,6 +169,9 @@ void ofApp::keyPressed(int key){
         server.send( toSend );
         messages.push_back("Sent: '" + toSend + "' to "+ ofToString(server.getConnections().size())+" websockets" );
         toSend = "";
+    }
+    if ( key == OF_KEY_ESCAPE ){
+            exit();
     }
 }
 
@@ -137,7 +199,17 @@ void ofApp::mousePressed(int x, int y, int button){
     url += "://localhost:" + ofToString( server.getPort() );
     ofLaunchBrowser(url);
 }
+void ofApp::newMidiMessage(ofxMidiMessage& msg) {
 
+	// make a copy of the latest message
+	midiMessage = msg;
+}
+void ofApp::exit() {
+
+	// clean up
+	midiIn.closePort();
+	midiIn.removeListener(this);
+}
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
 
@@ -154,6 +226,6 @@ void ofApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo){
 
 }
